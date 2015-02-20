6d16af2ca4ad68926e91a90145d68888f8ba18ed
Paul Chong <paulchong.nz@gmail.com>
2015-02-19 22:51:35 -0800

    first commit

--- Week2Assignment_Mailbox_v2/ViewController.swift
+++ Week2Assignment_Mailbox_v2/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Level2View: UIView!
+    var level2StartingX: CGFloat!
+    var level2StartingXPanBegan: CGFloat!
+    var finalLevel2PositionX: CGFloat!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        level2StartingX = Level2View.frame.origin.x
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,39 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func didPanLevel2(sender: UIPanGestureRecognizer) {
+        var location = sender.locationInView(view)
+        var translation = sender.translationInView(view)
+        var velocity = sender.velocityInView(view)
+
+        println("Velocity: \(velocity)")
+        println("Location: \(location)")
+        println("Translation: \(translation)")
+        if (sender.state == UIGestureRecognizerState.Began){
+            level2StartingXPanBegan = Level2View.frame.origin.x
+        } else if (sender.state == UIGestureRecognizerState.Changed){
+            finalLevel2PositionX = level2StartingXPanBegan + translation.x
+//            if (finalLevel2PositionX < level2StartingX){
+//                finalLevel2PositionX = level2StartingXPanBegan + translation.x
+//            }
+            Level2View.frame.origin.x = finalLevel2PositionX
+        } else if sender.state == UIGestureRecognizerState.Ended {
+            if (location.x > 160){
+                finalLevel2PositionX = 320
+//                trayRotation = CGFloat(-180.0 * M_PI/180)
+            } else {
+                finalLevel2PositionX = level2StartingX
+//                trayRotation = CGFloat(0 * M_PI/180)
+            }
+            UIView.animateWithDuration(0.2, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 30, options: nil, animations: { () -> Void in
+                self.Level2View.frame.origin.x = self.finalLevel2PositionX
+//                self.trayArrow.transform = CGAffineTransformMakeRotation(self.trayRotation)
+                }, completion: nil)
+            
+        }
+        
+    }
 
 }
 
